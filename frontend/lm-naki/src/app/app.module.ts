9af87cbbbf643c646c15a8b80f33c1a44efe691a
Zdenek Travnicek <travnicek@iim.cz>
2018-12-17 11:05:17 +0000

    Removed commented imports from app.module

--- frontend/lm-naki/src/app/app.module.ts
+++ frontend/lm-naki/src/app/app.module.ts
@@ -4,20 +4,16 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {NakiService} from './naki.service';
-// import {VideoContainerModule} from './video-container/video-container.module';
-// import {ImageContainerModule} from './image-container/image-container.module';
 import {AngularDraggableModule} from 'angular2-draggable';
 import {ContainerModule} from './container/container.module';
 import {MatButtonModule, MatButtonToggleModule, MatDialogModule, MatIconModule, MatInputModule, MatTableModule} from '@angular/material';
 import {NgDragDropModule} from 'ng-drag-drop';
 import {HttpClientModule} from '@angular/common/http';
 import {MenuModule} from './menu/menu.module';
-// import {ViewModule} from './view/view.module';
 
 import {AppRouterModule} from './app.router';
 import {HomeModule} from './home/home.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// import {FindItemComponent} from './find-item/find-item.component';
 import {FormsModule} from '@angular/forms';
 import {FindItemModule} from './find-item/find-item.module';
 import {NakiDefaultPlayerService} from './naki-default-player.service';
@@ -25,7 +21,6 @@ import {NakiDefaultPlayerService} from './naki-default-player.service';
 @NgModule({
   declarations: [
     AppComponent,
-    // FindItemComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,12 +29,9 @@ import {NakiDefaultPlayerService} from './naki-default-player.service';
     AppRouterModule,
     FormsModule,
 
-    // VideoContainerModule,
-    // ImageContainerModule,
     AngularDraggableModule,
     ContainerModule,
     MenuModule,
-    // ViewModule,
     HomeModule,
     FindItemModule,
 
@@ -58,10 +50,8 @@ import {NakiDefaultPlayerService} from './naki-default-player.service';
   ],
   bootstrap: [AppComponent],
   entryComponents: [
-    // FindItemComponent
   ],
   exports: [
-    // FindItemComponent
   ]
 })
 export class AppModule {
