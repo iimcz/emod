659a0ea0427c0333e6d4703a83fca90096198662
Zdenek Travnicek <travnicek@iim.cz>
2018-12-17 11:02:42 +0000

    Added missing import to container module

--- frontend/lm-naki/src/app/container/container.module.ts
+++ frontend/lm-naki/src/app/container/container.module.ts
@@ -7,6 +7,7 @@ import {GenericViewerModule} from '../generic-viewer/generic-viewer.module';
 import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
 import {IframeViewerModule} from '../iframe-viewer/iframe-viewer.module';
 import {VideoViewerModule} from '../video-viewer/video-viewer.module';
+import {ItemPlayerComponent} from './item-player/item-player.component';
 import {MotionViewerModule} from '../motion-viewer/motion-viewer.module';
 
 @NgModule({
