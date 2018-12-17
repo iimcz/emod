5bf63da3fff02257d3b7af7a2ee94377d70a3c00
Zdenek Travnicek <travnicek@iim.cz>
2018-12-17 10:47:42 +0000

    Removed unnecessary imports from container.module

--- frontend/lm-naki/src/app/container/container.module.ts
+++ frontend/lm-naki/src/app/container/container.module.ts
@@ -3,13 +3,10 @@ import {CommonModule} from '@angular/common';
 import {ContainerComponent} from './container/container.component';
 import {AngularDraggableModule} from 'angular2-draggable';
 import {ImageContainerModule} from '../image-container/image-container.module';
-import {ImageViewerComponent} from '../image-container/image-viewer/image-viewer.component';
 import {GenericViewerModule} from '../generic-viewer/generic-viewer.module';
 import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
 import {IframeViewerModule} from '../iframe-viewer/iframe-viewer.module';
-import {VideoViewComponent} from '../video-view/video-view/video-view.component';
 import {VideoViewModule} from '../video-view/video-view.module';
-import {ItemPlayerComponent} from './item-player/item-player.component';
 import {MotionContainerModule} from '../motion-container/motion-container.module';
 
 @NgModule({
@@ -32,10 +29,6 @@ import {MotionContainerModule} from '../motion-container/motion-container.module
   ],
   declarations: [ContainerComponent, ItemPlayerComponent],
   entryComponents: [
-    // ImageViewerComponent,
-    // GenericViewComponent,
-    // IframeViewComponent,
-    // VideoViewComponent
   ]
 })
 export class ContainerModule {
