41250fc0a3a1f76eafe7f8cc85c821ce594812a1
Zdenek Travnicek <travnicek@iim.cz>
2019-10-15 10:05:25 +0000

    Fixed link schema

--- backend/naki/naki/schemas/link.py
+++ backend/naki/naki/schemas/link.py
@@ -5,8 +5,11 @@ class LinkSchema(colander.MappingSchema):
     # item_id = colander.SchemaNode(colander.String())
     type = colander.SchemaNode(colander.String())
     uri = colander.SchemaNode(colander.String())
-    description = colander.SchemaNode(colander.String())
-    id_user = colander.SchemaNode(colander.String())
+    description = colander.SchemaNode(colander.String(), missing='')
+    id_user = colander.SchemaNode(colander.String(), missing='')
+    id_link = colander.SchemaNode(colander.String(), missing='')
+    id_item = colander.SchemaNode(colander.String(), missing='')
+
 
 class LinkSequenceSchema(colander.SequenceSchema):
     value = LinkSchema()
