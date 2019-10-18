27d0b8d85baa8002885552ff04628444866c3949
Zdenek Travnicek <travnicek@iim.cz>
2019-10-18 10:10:10 +0000

    Added type property to digital group interface and edit dialog

--- frontend/lm-naki/src/app/find-group/find-group.module.ts
+++ frontend/lm-naki/src/app/find-group/find-group.module.ts
@@ -5,15 +5,17 @@ import {EditGroupDialogComponent} from './edit-group-dialog/edit-group-dialog.co
 import {FindGroupComponent} from './find-group/find-group.component';
 import {FindGroupDialogComponent} from './find-group-dialog/find-group-dialog.component';
 import {FormsModule} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
 import {MetadataEditModule} from '../metadata-edit/metadata-edit.module';
 import {FindItemModule} from '../find-item/find-item.module';
+import {MatOptionModule} from '@angular/material/';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   imports: [
@@ -27,7 +29,8 @@ import {FindItemModule} from '../find-item/find-item.module';
     MatPaginatorModule,
     MatButtonModule,
     MatExpansionModule,
-
+    MatSelectModule,
+    MatOptionModule,
     MetadataEditModule,
     FindItemModule,
   ],
