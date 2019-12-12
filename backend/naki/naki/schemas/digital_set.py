68c91905bc80c86c9f373c3c1ce0be6f4646c588
Zdenek Travnicek <travnicek@iim.cz>
2019-12-12 15:46:20 +0000

    Replaced generated schemas with manually created ones

--- backend/naki/naki/schemas/digital_set.py
+++ backend/naki/naki/schemas/digital_set.py
@@ -3,15 +3,25 @@ from colanderalchemy import SQLAlchemySchemaNode
 from naki.schemas.metadata import MetadataSequenceSchema
 from naki.schemas.digital_group import DigitalGroupSchema
 import colander
+import datetime
 
 
 class DGroupSchema(colander.MappingSchema):
     id_group = colander.SchemaNode(colander.String())
 
+
 class DGroupSequenceSchema(colander.SequenceSchema):
     group = DGroupSchema()
 
 
-DigitalSetSchema = SQLAlchemySchemaNode(DigitalSet)
-DigitalSetSchema.add(MetadataSequenceSchema(name='metadata', missing=[]))
-DigitalSetSchema.add(DGroupSequenceSchema(name='groups', missing=[]))
+class DigitalSetSchema(colander.MappingSchema):
+    id_set = colander.SchemaNode(colander.String(), missing='')
+    created = colander.SchemaNode(colander.DateTime(), missing=datetime.datetime.now())
+    description = colander.SchemaNode(colander.String(), missing='')
+    id_user = colander.SchemaNode(colander.String(), missing='')
+    metadata = MetadataSequenceSchema(missing=[])
+    groups = DGroupSequenceSchema(missing=[])
+
+# DigitalSetSchema = SQLAlchemySchemaNode(DigitalSet)
+# DigitalSetSchema.add(MetadataSequenceSchema(name='metadata', missing=[]))
+# DigitalSetSchema.add(DGroupSequenceSchema(name='groups', missing=[]))
