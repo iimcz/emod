713d89996932e31e79209f03ad8cc3e560385890
Zdenek Travnicek <travnicek@iim.cz>
2019-12-16 19:10:47 +0100

    Added support for newer zope transaction

--- backend/naki/naki/model/meta.py
+++ backend/naki/naki/model/meta.py
@@ -1,12 +1,17 @@
 """SQLAlchemy Metadata and Session object"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from zope.sqlalchemy import ZopeTransactionExtension
 
 __all__ = ['Base', 'Session']
 
 # SQLAlchemy session manager. Updated by model.init_model()
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+try:
+    from zope.sqlalchemy import ZopeTransactionExtension
+    DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+except:
+    from zope.sqlalchemy import register
+    DBSession = scoped_session(sessionmaker(autoflush=True))
+    register(DBSession)
 
 # The declarative Base
 Base = declarative_base()
